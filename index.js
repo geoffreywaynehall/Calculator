2c01fc3225106ad5c681a1e8c5c1c24da597da90
Geoffrey Hall <geoffreywaynehall@gmail.com>
2021-10-14 15:59:27 -0400

    Adding and initializing React

---
+++ index.js
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import './index.css';
+
+class App extends React.Component {
+    state = {};
+
+    render() {
+        return (
+            <div></div>    
+        );
+    }
+}
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('app')
+)
