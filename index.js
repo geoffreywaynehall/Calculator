462ac6150ace7883e730cb1bd165f1734e8a48f5
Geoffrey Hall <geoffreywaynehall@gmail.com>
2021-10-15 13:07:53 -0400

    Basic styling

--- index.js
+++ index.js
@@ -3,11 +3,20 @@ import * as ReactDOM from 'react-dom';
 import './index.css';
 
 class App extends React.Component {
-    state = {};
+    state = {
+        theme: 1
+    };
 
     render() {
         return (
-            <div></div>    
+            <div id="calculator">
+                <h1>calc</h1>
+                <div class="theme">
+                    <h2>THEME</h2><h2>1</h2><h2>2</h2><h2>3</h2>
+                </div>
+                <div class="screen"></div>
+                <div class="pad"></div>
+            </div>    
         );
     }
 }
