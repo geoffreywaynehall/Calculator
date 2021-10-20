c66f547fceb1707f4ef3aca7574b5f0f2df7045a
Geoffrey Hall <geoffreywaynehall@gmail.com>
2021-10-20 10:56:27 -0400

    Adding number presses, display formatting, del, and reset

--- index.js
+++ index.js
@@ -5,52 +5,90 @@ import './index.css';
 class App extends React.Component {
     state = {
         theme: 1,
-        display: "399,981"
+        display: "0"
     };
+
+    numberPress = (num) => {
+        let oldDisplay = this.state.display;
+        if (oldDisplay === "0") {
+            this.setState({
+                display: num
+            });
+        } else {
+            this.setState({
+                display: this.formatDisplay(oldDisplay + num)
+            });
+        }
+    }
+
+    reset = () => {
+        this.setState({
+            display: "0"
+        });
+    }
+
+    del = () => {
+        if (this.state.display.length === 1) {
+            this.setState({
+                display: "0"
+            });
+        } else {
+            this.setState({
+                display: this.formatDisplay(this.state.display.slice(0, -1))
+            });
+        }
+
+    }
+
+    formatDisplay = (num) => {
+        var str = num.replaceAll(',', '').split(".");
+        str[0] = str[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+        return str.join(".");
+    }
 
     render() {
         return (
             <div id="calculator">
                 <h1>calc</h1>
-                <div class="theme">
-                    <div class="theme-upper">
-                        <h2 class="theme-number">1</h2><h2 class="theme-number">2</h2><h2 class="theme-number">3</h2>
+                <div className="theme">
+                    <div className="theme-upper">
+                        <h2 className="theme-number">1</h2><h2 className="theme-number">2</h2><h2 className="theme-number">3</h2>
                     </div>
-                    <div class="theme-lower">
-                        <h2 class="theme-title">THEME</h2><div className="slider"><div class="dot"></div><div class="dot"></div><div class="dot"></div></div>
+                    <div className="theme-lower">
+                        <h2 className="theme-title">THEME</h2><div className="slider"><div class="dot"></div><div className="dot"></div><div className="dot"></div></div>
                     </div>
                 </div>
-                <div class="screen">
-                    <p class="display">{ this.state.display }</p>
+                <div className="screen">
+                    <p className="display">{ this.state.display }</p>
                 </div>
-                <div class="pad">
-                    <div class="row">
-                        <div class="button number">7</div>
-                        <div class="button number">8</div>
-                        <div class="button number">9</div>
-                        <div class="button blue">DEL</div>
+                <div className="pad">
+                    <div className="row">
+                        <div className="button number" onClick={() => { this.numberPress("7"); }}>7</div>
+                        <div className="button number" onClick={() => { this.numberPress("8"); }}>8</div>
+                        <div className="button number" onClick={() => { this.numberPress("9"); }}>9</div>
+                        <div className="button blue" onClick={() => { this.del(); }}>DEL</div>
                     </div>
-                    <div class="row">
-                        <div class="button number">4</div>
-                        <div class="button number">5</div>
-                        <div class="button number">6</div>
-                        <div class="button">+</div>
+                    <div className="row">
+                        <div className="button number" onClick={() => { this.numberPress("4"); }}>4</div>
+                        <div className="button number" onClick={() => { this.numberPress("5"); }}>5</div>
+                        <div className="button number" onClick={() => { this.numberPress("6"); }}>6</div>
+                        <div className="button">+</div>
                     </div>
-                    <div class="row">
-                        <div class="button number">1</div>
-                        <div class="button number">2</div>
-                        <div class="button number">3</div>
-                        <div class="button">-</div>
+                    <div className="row">
+                        <div className="button number" onClick={() => { this.numberPress("1"); }}>1</div>
+                        <div className="button number" onClick={() => { this.numberPress("2"); }}>2</div>
+                        <div className="button number" onClick={() => { this.numberPress("3"); }}>3</div>
+                        <div className="button">-</div>
                     </div>
-                    <div class="row">
-                        <div class="button">.</div>
-                        <div class="button number">0</div>
-                        <div class="button">/</div>
-                        <div class="button">x</div>
+                    <div className="row">
+                        <div className="button">.</div>
+                        <div className="button number" onClick={() => { this.numberPress("0"); }}>0</div>
+                        <div className="button">/</div>
+                        <div className="button">x</div>
                     </div>
-                    <div class="row">
-                        <div class="button wide blue">RESET</div>
-                        <div class="button wide red">=</div>
+                    <div className="row">
+                        <div className="button wide blue" onClick={() => { this.reset(); }}>RESET</div>
+                        <div className="button wide red">=</div>
                     </div>
                 </div>
             </div>    
